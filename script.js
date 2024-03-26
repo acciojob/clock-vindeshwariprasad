2d1203c8f235192ff3e2f4ed8effff3916d91031
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-03-26 18:24:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Function to update the timer
+function updateTimer() {
+    var timerElement = document.getElementById('timer');
+    var now = new Date();
+    var dateTimeString = now.toLocaleString();
+    timerElement.textContent = dateTimeString;
+}
+
+
+updateTimer();
+setInterval(updateTimer, 1000);
+
